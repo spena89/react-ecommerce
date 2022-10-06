804dafd950e9698cbd2d1a15e86396b38304f780
santiago Pena <psanti89@gmail.com>
2022-10-06 16:44:38 -0300

    firebase branch

--- src/Containers/Cart/Cart.jsx
+++ src/Containers/Cart/Cart.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useCartContext } from "../../Context/CartContext";
 import {
     CardContent,
@@ -10,7 +10,8 @@ import {
 import DeleteIcon from "@mui/icons-material/Delete";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import "./Cart.css"
+import Fade from "@mui/material/Fade";
+import "./Cart.css";
 
 const Cart = () => {
     const { cartList, totalPrice, removeProduct, emptyCart } = useCartContext();
@@ -19,50 +20,59 @@ const Cart = () => {
     return (
         <div>
             {cartList.map((prod) => (
-                <Card  sx={{ minWidth: 275, margin: "6px" }} key={prod.id}>
-                    <Grid className="cartItem"
-                        container
-                        spacing={0}
-                        alignItems="center"
-                        justifyContent="center">
-                        <Grid item xs={8}>
-                            <CardContent>
-                                <Typography
-                                    sx={{ fontSize: 14 }}
-                                    color="text.secondary"
-                                    gutterBottom>
-                                    {prod.title}
-                                </Typography>
-                                <CardMedia
-                                    component="img"
-                                    alt={prod.title}
-                                    image={prod.image}
-                                    sx={{
-                                        padding: "1em 1em 0 1em",
-                                        width: "5%",
-                                    }}
+                <Card sx={{ minWidth: 275, margin: "6px"}} key={prod.id}>
+                    <Fade in="true" timeout={700}>
+                        <Grid
+                            className="cartItem"
+                            container
+                            spacing={0}
+                            alignItems="center"
+                            justifyContent="center">
+                            <Grid item xs={8}>
+                                <CardContent>
+                                    <Typography
+                                        sx={{ fontSize: 14 }}
+                                        color="text.secondary"
+                                        gutterBottom>
+                                        {prod.title}
+                                    </Typography>
+                                    <CardMedia
+                                        component="img"
+                                        alt={prod.title}
+                                        image={prod.image}
+                                        sx={{
+                                            padding: "1em 1em 0 1em",
+                                            width: "5%",
+                                        }}
+                                    />
+                                    <Typography
+                                        sx={{ fontSize: 14 }}
+                                        color="text.secondary"
+                                        gutterBottom>
+                                        Precio por unidad: ${prod.price}
+                                    </Typography>
+                                    <Typography
+                                        sx={{ fontSize: 14 }}
+                                        color="text.secondary"
+                                        gutterBottom>
+                                        Cantidad: {prod.quantity}
+                                    </Typography>
+                                </CardContent>
+                            </Grid>
+
+                            <Grid item xs={4}>
+                                <DeleteIcon
+                                    onMouseEnter={() => setHover(true)}
+                                    onMouseLeave={() => setHover(false)}
+                                    aria-label="delete"
+                                    onClick={() => removeProduct(prod.id)}
                                 />
-                                <Typography
-                                    sx={{ fontSize: 14 }}
-                                    color="text.secondary"
-                                    gutterBottom>
-                                    Precio por unidad: ${prod.price}
-                                </Typography>
-                                <Typography
-                                    sx={{ fontSize: 14 }}
-                                    color="text.secondary"
-                                    gutterBottom>
-                                    Cantidad: {prod.quantity}
-                                </Typography>
-                            </CardContent>
+                                {inHover && (
+                                    <span className="delete">Delete?</span>
+                                )}
+                            </Grid>
                         </Grid>
-                        <Grid item xs={4}>
-                            <DeleteIcon onMouseEnter={()=>setHover(true)} onMouseLeave={()=> setHover(false)}
-                                aria-label="delete"
-                                onClick={() => removeProduct(prod.id)}/>
-                                {inHover && <span className="delete">Delete?</span>}
-                        </Grid>
-                    </Grid>
+                    </Fade>
                 </Card>
             ))}
             {cartList.length === 0 ? (
@@ -86,7 +96,7 @@ const Cart = () => {
                             variant="contained"
                             color="error"
                             size="small"
-                            onClick={() => emptyCart()} >
+                            onClick={() => emptyCart()}>
                             Vaciar carrito
                         </Button>
                         <p>Cart Total : USD ${totalPrice()}</p>
